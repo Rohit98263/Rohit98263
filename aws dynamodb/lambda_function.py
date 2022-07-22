7bc5f50af29345599bf8a56d279a767b226d76a2
Rohitkumar Singh <rosingh@osius.com>
2022-07-22 14:16:21 +0530

    dynamodb operation

---
+++ aws dynamodb/lambda_function.py
@@ -0,0 +1,24 @@
+import json
+from dynamodbclass import Dynamodb
+from datetime import timezone
+import datetime
+
+def lambda_handler(event, context):
+    try:
+        #data storing in dynmaodb
+        data=event['arguments']['input']
+        data['date']=datetime.datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+        
+        #putting data in repective table using application no(partition key) and date(sort key) and extracting tablename from event '
+        if  event['info']['fieldName']=='createDeliverable':
+            tablename='Deliverables'
+        elif event['info']['fieldName']=='createRaiseConcern':
+            tablename='RaiseConcern'
+        else:
+            tablename='ChatBox'
+        a=Dynamodb()
+        res=a.putitem(tablename,data)
+        
+        return data
+    except Exception as e:
+        raise e
---
+++ main.py
@@ -0,0 +1,3 @@
+if __name__=="__main__":
+    print(f"testing git")
+print(f"outside main")

ace78016fbf079a5325a42221e026e19864ccf8a
Rohitkumar Singh <rosingh@osius.com>
2022-07-22 14:28:27 +0530

    for access token for authenticate user

---
+++ change status on db table/lambdafunction1.py
@@ -0,0 +1,25 @@
+import json
+from dynamodb import Dynamodb
+from boto3.dynamodb.types import TypeDeserializer
+import boto3
+
+def lambda_handler(event, context):
+    
+    # try:
+        application_no=event['arguments']['application_no']
+        
+        #Retriving data from repective table from the event and using application_no as a partition key
+        if  event['info']['fieldName']=='listDeliverables':
+            tablename='Deliverables'
+        elif event['info']['fieldName']=='listRaiseConcern':
+            tablename='RaiseConcern'
+        else:
+            tablename='ChatBox'
+        a=Dynamodb()
+        dict={'application_no':application_no}
+        res=a.querywithsorting(tablename,dict)
+    
+        return {"items":res}
+        
+    # except Exception as e :
+    #   raise e 

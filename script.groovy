e1c18c1d51edfdcfec8d406768c13bbedece82c8
Rohit98263 <rohit.skn98@gmail.com>
2022-10-12 19:07:20 +0530

    test

---
+++ script.groovy
@@ -0,0 +1,12 @@
+Host gitlab.com
+    User Rohitkumar Singh
+    HostName gitlab.com
+    PreferredAuthentications publickey
+    IdentityFile /home/osiuser/.ssh/id_rsa
+Host gitlab.com
+    User ROHIT SINGH
+    HostName gitlab.com
+    PreferredAuthentications publickey
+    IdentityFile /home/osiuser/.ssh/id_rsa
+
+
